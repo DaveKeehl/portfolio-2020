3a9270b0bc2723824df8fd2c9ed8fbedcfac737a
Davide <davide.ciulla@ticino.com>
2020-06-24 01:14:48 +0200

    Update

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,8 +7,16 @@ import Footer from "../Footer"
 
 function Contact() {
 	return (
-		<div className="contact" id="contact">
+		<div className="contact">
 			<Container>
+				<div
+					id="contact"
+					style={{
+						position: "absolute",
+						top: "50px",
+						opacity: "0"
+					}}
+				></div>
 				<h1
 					data-sal="fade"
 					data-sal-duration="800"
@@ -27,13 +35,36 @@ function Contact() {
 					data-sal-easing="ease"
 				>
 					<label htmlFor="name">Full name</label>
-					<input id="name" type="text" name="name" required />
+					<input
+						id="name"
+						type="text"
+						name="name"
+						placeholder="e.g. John Doe"
+						required
+					/>
 					<label htmlFor="email">Email</label>
-					<input id="email" type="text" name="email" required />
-					{/* <label htmlFor="subject">Subject</label>
-					<input id="subject" type="text" name="subject" required /> */}
+					<input
+						id="email"
+						type="text"
+						name="email"
+						placeholder="e.g. john-doe@email.com"
+						required
+					/>
+					<label htmlFor="subject">Subject</label>
+					<input
+						id="subject"
+						type="text"
+						name="subject"
+						placeholder="e.g. I have an idea for an app"
+						required
+					/>
 					<label htmlFor="message">Message</label>
-					<textarea id="message" required />
+					<textarea
+						id="message"
+						name="text"
+						placeholder="e.g. Hi, my name is John Doe. I was thinking..."
+						required
+					/>
 					<input type="submit" value="Send" />
 				</form>
 				<Footer />
