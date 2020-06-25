c21bb9b2e9476f044859bbb1fff8162afe757c0e
Davide <davide.ciulla@ticino.com>
2020-06-25 12:33:17 +0200

    Important update

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,11 +5,24 @@ import Container from "../Container"
 
 import json from "../../../static/content.json"
 import arrow from "../../images/arrow.svg"
+import circle from "../../images/circle.svg"
+import rectangle from "../../images/rectangle.svg"
+import triangle from "../../images/triangle.svg"
+import dave from "../../images/dave.jpg"
 
 function Hero() {
 	return (
 		<Container>
 			<div className="hero" id="hero">
+				<div className="hero__shapes">
+					<img src={circle} alt="circle" />
+					<img src={rectangle} alt="rectangle" />
+					<img src={triangle} alt="triangle" />
+					<div className="hero__shapes__dave">
+						<img src={dave} alt="dave" />
+						<img src={dave} alt="dave" />
+					</div>
+				</div>
 				<div className="hero__main">
 					<h1 className="hero__value">
 						{json.hero.value_proposition}
