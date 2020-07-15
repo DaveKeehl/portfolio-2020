5061d3c9768c01355d8a213a84d7016d406404a9
Davide <davide.ciulla@ticino.com>
2020-07-15 22:15:59 +0200

    icons cleanup

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,8 +1,8 @@
-import React from "react"
+import React from 'react'
 
-import Container from "../Container"
+import Container from '../Container'
 
-import json from "../../../static/content.json"
+import json from '../../../static/content.json'
 
 function About() {
 	return (
